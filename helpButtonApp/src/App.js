15fe8b85176fd356a7692f521022becc8a3239d3
Thomas Chester <thomas.charles.chester@gmail.com>
2017-08-22 16:16:04 -0500

    initi

--- helpButtonApp/src/App.js
+++ helpButtonApp/src/App.js
@@ -1,19 +1,11 @@
-
-import React, { PropTypes, Component } from 'react';
-import classnames from 'classnames';
-
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './style.css';
+import './App.css';
 
 class App extends Component {
-  // static propTypes = {}
-  // static defaultProps = {}
-  // state = {}
-
   render() {
-    const { className, ...props } = this.props;
     return (
-      <div className={classnames('App', className)} {...props}>
+      <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
