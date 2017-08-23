1bab59f7d9961bcb200ade7f6d0c83c274d82fbb
Thomas Chester <thomas.charles.chester@gmail.com>
2017-08-23 16:18:21 -0500

    nothing really

--- helpButtonApp/src/index.js
+++ helpButtonApp/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+<Router history={browserHistory}>
+  <Route path="/" component={Root}>
+  </Route>
+</Router>
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
